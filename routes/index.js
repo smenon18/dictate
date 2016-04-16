7512346ec0add3733723e4d430f8ad42665724ad
Shaan Menon <smenon18@gmail.com>
2016-04-16 16:39:37 -0400

    added bootstrap css, animate for noty, and routed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* HTML Files */
+router.get('/', function(req, res) {
+  res.sendFile(__dirname + 'index.html');
+});
+
+router.get('/login', function(req, res){
+    res.sendFile(__dirname + '/login.html');
+});
+
+/* JS Files */
+router.get('/jquery', function(req, res){
+   res.sendFile(__dirname + 'js/jquery-2.1.4.min.js');
+});
+
+router.get('/noty', function(req, res){
+   res.sendFile(__dirname + 'js/jquery.noty.packaged.min.js');
+});
+
+router.get('/bootstrapjs', function(req, res){
+    res.sendFile(__dirname + 'js/bootstrap.min.js');
+});
+
+/* CSS Files */
+router.get('/bootstrapcss',function(req, res){
+    res.sendFile(__dirname + 'css/bootstrap.min.css');
+});
+
+router.get('/animate', function(req,res){
+    res.sendFile(__dirname + 'css/Animate.css');
 });
 
 module.exports = router;
