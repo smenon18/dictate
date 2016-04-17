8434d14c703ea8f3a67d406c290889f8ca95606c
Shaan Menon <smenon18@gmail.com>
2016-04-17 05:53:52 -0400

    added contacts table

--- models/database.js
+++ models/database.js
@@ -17,8 +17,15 @@ var user = 'CREATE TABLE user (id SERIAL PRIMARY KEY, ' +
             'username VARCHAR(32) NOT NULL UNIQUE, ' +
 				    'pass VARCHAR(128) NOT NULL, ' +
 				    'salt VARCHAR(8) NOT NULL);';
+                    
+var contact = 'CREATE TABLE contact (' +
+                'user1 serial NOT NULL, ' +
+                'user2 serial NOT NULL, ' +
+                'PRIMARY KEY(user1,user2), ' +
+                'FORIEGN KEY(user1) REFERENCES user(id), ' +
+                'FORIEGN KEY(user2) REFERENCES user(id));'
 
-var query = client.query(reset + user);
+var query = client.query(reset + user + contact);
 query.on('end', function() {
   client.end();
 });
