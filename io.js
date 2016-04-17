942a1e7099ef6776733d15eb06a3504c711ccfbd
John Bucknam <jbuck594@gmail.com>
2016-04-16 20:51:37 -0400

    Edited demo.html

--- io.js
+++ io.js
@@ -2,4 +2,12 @@ var Server = require('socket.io');
 
 var io = new Server();
 
+io.sockets.on('connection',
+function(socket)
+{
+    socket.on('adduser', function(data)
+    {
+    });
+});
+
 module.exports = io;
